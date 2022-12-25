21d1d8e8ab32e075ae10ce80952b4907b7fa8132
anishjoshi1999 <anishjoshi1999@gmail.com>
2022-12-25 14:49:38 +0545

    max_tokens updated

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.post('/openai/chatgpt',async(req,res)=> {
             model: "text-davinci-003",
             prompt: `${prompt}`,
             temperature: 0,
-            max_tokens: 64,
+            max_tokens: 3000,
             top_p: 1,
             frequency_penalty: 0.5,
             presence_penalty: 0
